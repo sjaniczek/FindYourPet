2701fac1175b22b9ec4726c027696ef6765a0db1
Sebastian <sjaniczek97@gmail.com>
2023-06-26 01:52:06 +0200

    LostCreate almost done

--- app/src/main/java/com/edu/wszib/findyourpet/LostCreateFragment.kt
+++ app/src/main/java/com/edu/wszib/findyourpet/LostCreateFragment.kt
@@ -2,17 +2,55 @@ package com.edu.wszib.findyourpet
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.edu.wszib.findyourpet.databinding.FragmentCreateLostBinding
+import com.edu.wszib.findyourpet.models.LostPetData
+import com.edu.wszib.findyourpet.models.LostPetViewModel
 
 class LostCreateFragment : Fragment() {
 
     private var _binding: FragmentCreateLostBinding? = null
     private val binding: FragmentCreateLostBinding
         get() = _binding!!
+    private var imageUrl: String? = null
+    private var imageUri: Uri? = null
+    private val lostPetViewModel: LostPetViewModel by activityViewModels()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Log.i("permission", "Permission granted")
+                launchImagePicker()
+            } else {
+                Log.i("permission", "Permission denied")
+                // Handle permission denied case
+            }
+        }
+    private val getImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                imageUri = result.data?.data
+                imageUrl = imageUri.toString()
+                binding.ivLostPet.setImageURI(imageUri)
+            }
+        }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +60,67 @@ class LostCreateFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.buttonChooseLostPic.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                // For API 33 and above
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                    Log.i("permission", "Requesting permission")
+                } else {
+                    Log.i("permission", "Permission already granted")
+                    launchImagePicker()
+                }
+            } else {
+                // For API below 33
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    Log.i("permission", "Requesting permission")
+                } else {
+                    Log.i("permission", "Permission already granted")
+                    launchImagePicker()
+                }
+            }
+        }
             binding.buttonGoToMap.setOnClickListener{
+                saveFormData()
                 findNavController().navigate(LostCreateFragmentDirections.actionLostCreateFragmentToLostMapsFragment())
             }
+        val lostPetData = lostPetViewModel.lostPetData
+        if(lostPetData != null)
+        {
+            binding.etLostAddress.setText(lostPetData.decodedAddress)
+        }
     }
+
+    private fun saveFormData() {
+        val lostPetData = LostPetData(
+            petName = binding.etLostPetName.text.toString(),
+            petType = binding.rgLostType.findViewById<RadioButton>(binding.rgLostType.checkedRadioButtonId)?.text.toString(),
+            petReact = binding.rgLostReacts.findViewById<RadioButton>(binding.rgLostReacts.checkedRadioButtonId)?.text.toString(),
+            petBehavior = binding.rgLostBehavior.findViewById<RadioButton>(binding.rgLostBehavior.checkedRadioButtonId)?.text.toString(),
+            petAge = binding.etLostAge.text.toString(),
+            decodedAddress = binding.etLostAddress.text.toString(),
+            additionalPetInfo = binding.tvLostPetAdditionalInfo.text.toString()
+        )
+
+        lostPetViewModel.saveFormData(lostPetData)
+    }
+
+    private fun launchImagePicker() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        getImageLauncher.launch(intent)
+    }
+
+
 }
--- app/src/main/java/com/edu/wszib/findyourpet/LostMapsFragment.kt
+++ app/src/main/java/com/edu/wszib/findyourpet/LostMapsFragment.kt
@@ -1,34 +1,58 @@
 package com.edu.wszib.findyourpet
 
+import android.location.Address
+import android.location.Geocoder
+import android.os.Build
 import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.edu.wszib.findyourpet.databinding.FragmentLostMapsBinding
+import com.edu.wszib.findyourpet.models.LostPetData
+import com.edu.wszib.findyourpet.models.LostPetViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import java.util.*
 
-class LostMapsFragment : Fragment() {
+class LostMapsFragment : Fragment(), OnMapReadyCallback {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private val lostPetViewModel: LostPetViewModel by activityViewModels()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var googleMap: GoogleMap
+    private var _binding: FragmentLostMapsBinding? = null
+    private val binding get() = _binding!!
+
+    private val requestLocationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            getCurrentLocation()
+        } else {
+            // Permission denied, show a message or do something else
+            Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     override fun onCreateView(
@@ -36,12 +60,122 @@ class LostMapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_lost_maps, container, false)
+        _binding = FragmentLostMapsBinding.inflate(inflater, container, false)
+
+        val lostPetViewModel = lostPetViewModel.lostPetData
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_lost_layout) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        googleMap.uiSettings.setAllGesturesEnabled(true)
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(5.0F))
+
+        getCurrentLocation()
+        binding.buttonLostMapSend.setOnClickListener {
+            val currentLocation = googleMap.cameraPosition.target
+            decodeLocation(currentLocation)
+        }
+    }
+
+    private fun decodeLocation(currentLocation: LatLng) {
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val addresses = geocoder.getFromLocation(
+                currentLocation.latitude,
+                currentLocation.longitude,
+                1,
+                object : Geocoder.GeocodeListener {
+                    override fun onGeocode(addresses: MutableList<Address>) {
+                        val decodedAddress = addresses[0].getAddressLine(0)
+                        lostPetViewModel.lostPetData?.decodedAddress = decodedAddress
+                        lostPetViewModel.lostPetData?.lostLocation = LostPetData.LostLocation(
+                            currentLocation.latitude,
+                            currentLocation.longitude
+                        )
+                        Log.i("decodeLocation",decodedAddress.toString())
+                        Log.i("decodeLocation",lostPetViewModel.lostPetData?.decodedAddress.toString())
+                        Log.i("decodeLocation",lostPetViewModel.toString())
+                        Log.i("decodeLocation",lostPetViewModel.lostPetData?.lostLocation.toString())
+                        activity?.runOnUiThread {
+                            Toast.makeText(
+                                context,
+                                lostPetViewModel.lostPetData?.decodedAddress.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        findNavController().navigate(LostMapsFragmentDirections.actionLostMapsFragmentToLostCreateFragment())
+                    }
+
+                    override fun onError(errorMessage: String?) {
+                        super.onError(errorMessage)
+                        Toast.makeText(
+                            context,
+                            "Błąd w pobieraniu adresu. Spróbuj ponownie później lub wpisz adres ręcznie.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                })
+        } else {
+            @Suppress("DEPRECATION")
+            val addresses =
+                geocoder.getFromLocation(currentLocation.latitude, currentLocation.longitude, 1)
+            if (addresses != null && addresses.isNotEmpty()) {
+                // Process the retrieved addresses
+                val decodedAddress = addresses[0].getAddressLine(0)
+                lostPetViewModel.lostPetData?.decodedAddress = decodedAddress
+                lostPetViewModel.lostPetData?.lostLocation =
+                    LostPetData.LostLocation(currentLocation.latitude, currentLocation.longitude)
+
+                findNavController().navigate(LostMapsFragmentDirections.actionLostMapsFragmentToLostCreateFragment())
+            } else {
+                Toast.makeText(
+                    context,
+                    "Błąd w pobieraniu adresu. Spróbuj ponownie później lub wpisz adres ręcznie.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun getCurrentLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            fusedLocationClient.getCurrentLocation(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                object : CancellationToken() {
+                    override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                        CancellationTokenSource().token
+
+                    override fun isCancellationRequested() = false
+                }).addOnSuccessListener { location: Location? ->
+                Log.i("userloc", location.toString())
+                if (location == null)
+                    Toast.makeText(context, "Cannot get location.", Toast.LENGTH_SHORT).show()
+                else {
+                    val lat = location.latitude
+                    val lng = location.longitude
+                    val latLng = LatLng(lat, lng)
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                    googleMap.animateCamera(CameraUpdateFactory.zoomTo(6F))
+                }
+            }
+        }//WHAT IF DENIED?
     }
 }
